d968013117202ee70cdf72acccb25ea14d6f2797
Viktor <viktor.b@ramotion.agency>
2020-01-10 18:02:44 +0300

    Add image

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
 import styles from './style.scss';
+import Icon from './icons8-github.png';
 
 function component() {
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', ' ', 'webpack'], '');
     element.classList.add('red');
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
     return element;
 }
 
