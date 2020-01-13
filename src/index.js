577332d4ab10a05e146b38210d64b6e2ddf73ff6
Viktor <viktor.b@ramotion.agency>
2020-01-13 16:03:20 +0300

    Use webpack development tools.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import _ from 'lodash';
+import print from './print.js';
+
+
 import styles from './style.scss';
-import Icon from './icons8-github.png';
+import octoCat from './icons8-github.png';
+
 
 function component() {
     const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', ' ', 'webpack'], '');
+    const image = new Image();
+    const button = document.createElement('button');
+
+    element.innerHTML = _.join(['Hello', ' ', 'webpack'], ' ');
     element.classList.add('red');
 
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
+    image.src = octoCat;
+
+    button.innerHTML = 'Tap to print into console';
+    button.onclick = print;
+
+    element.appendChild(button);
+    element.appendChild(image);
 
     return element;
 }
---
+++ src/print.js
@@ -0,0 +1,3 @@
+export default function print() {
+    console.log('Function \"print\" is called.\nNow we have our own server!');
+}
