b5473364be834c1a7815455cbe874d94c74b1f6b
Viktor <viktor.b@ramotion.agency>
2020-01-13 17:45:59 +0300

    Add some buttons

--- src/homePage/index.js
+++ src/homePage/index.js
@@ -9,18 +9,30 @@ import octoCat from './icons8-github.png';
 function component() {
     const element = document.createElement('div');
     const image = new Image();
-    const button = document.createElement('button');
+    const printButton = document.createElement('button');
+    const firstButton = document.createElement('button');
+    const secondButton = document.createElement('button');
+    const thirdButton = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', ' ', 'webpack!'], ' ');
     element.classList.add('red');
 
     image.src = octoCat;
 
-    button.innerHTML = 'Tap to print into console';
-    button.onclick = print;
+    printButton.innerHTML = 'Tap to print into console';
+    printButton.onclick = print;
 
-    element.appendChild(button);
+    firstButton.innerHTML = 'Go to first page';
+
+    secondButton.innerHTML = 'Go to second page';
+
+    thirdButton.innerHTML = 'Go to third page';
+
     element.appendChild(image);
+    element.appendChild(printButton);
+    element.appendChild(firstButton);
+    element.appendChild(secondButton);
+    element.appendChild(thirdButton);
 
     return element;
 }
