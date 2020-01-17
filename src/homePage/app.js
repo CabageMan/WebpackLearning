12aba4d7e3aae35e59c5237132406be167818502
Viktor <viktor.b@ramotion.agency>
2020-01-17 16:28:44 +0300

    Small improvements.

--- src/homePage/app.js
+++ src/homePage/app.js
@@ -6,58 +6,6 @@ import * as issueForm from './formBlock';
 import styles from './style.scss';
 import octoCat from './icons8-github.png';
 
-
-
-function component() {
-    const element = document.createElement('div');
-    const image = new Image();
-    const printButton = document.createElement('button');
-    
-    const secondButton = document.createElement('button');
-    const thirdButton = document.createElement('button');
-
-    element.innerHTML = _.join(['Hello', ' ', 'webpack!'], ' ');
-    element.classList.add('red');
-
-    image.src = octoCat;
-
-    printButton.innerHTML = 'Tap to print into console';
-    printButton.onclick = print;
-
-    secondButton.innerHTML = 'Go to second page';
-
-    thirdButton.innerHTML = 'Go to third page';
-
-    element.appendChild(image);
-    element.appendChild(printButton);
-    element.appendChild(createFirstButton());
-    element.appendChild(secondButton);
-    element.appendChild(thirdButton);
-
-    return element;
-}
-
-function createFirstButton() {
-    const button = document.createElement('button');
-    button.innerHTML = 'Go to First Page';
-    button.onclick = onFirstButtonClick;
-    return button;
-}
-
-function onFirstButtonClick() {
-    window.location = /about/;
-}
-
-// function createA() {
-//     const link = document.createElement('a');
-//     link.title = 'Go to About';
-//     link.innerHTML = 'Go to About';
-//     link.href = "/about/";
-//     link.classList.add('button');
-//     return link;
-// }
-
-// document.body.appendChild(component());
 function createIssueComponent() {
     const element = document.createElement('div');
     
@@ -69,6 +17,7 @@ function createIssueComponent() {
 
 document.body.appendChild(createIssueComponent());
 
+// Need to improve form creating. It's not clear where ID of button is set.
 document.getElementById("issueSubmitButton").addEventListener("click", function(event){
     event.preventDefault();
     issueForm.validateIssueForm();
--- src/homePage/formElements.js
+++ src/homePage/formElements.js
@@ -57,22 +57,3 @@ export const createSubmitButton = (title, buttonID) => {
 
     return button;
 };
-
-// const createAndAppendCard = (header, body) => {
-//     const cardFragment = document.createDocumentFragment();
-//     const cardElement = document.createElement('div');
-//     const cardHeader = `<h2 class="card-header">${header}</h2>`;
-//     const cardBody = `<div class='card-body">${body}</div>`;
-
-//     cardElement.innerHTML = `
-//         <div class="card">
-//             ${cardHeader}
-//             ${cardBody}
-//         </div>
-//     `;
-
-//     cardFragment.appendChild(cardElement);
-//     document.body.appendChild(cardFragment);
-// };
-
-// export default createAndAppendCard
